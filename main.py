9af339540f846bab610a74b8e30176fb40afd8f9
Sérgio Vorniches <vorniches@gmail.com>
2023-01-24 22:08:37 +0000

    basic version created

---
+++ main.py
@@ -0,0 +1,26 @@
+import openai
+
+
+def authenticate():
+    return input("Please enter your OpenAI API key: ")
+
+
+def chat():
+    prompt = input("You can now enter your prompt: ")
+    completions = openai.Completion.create(
+        engine="davinci",
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    message = completions.choices[0].text
+    print(message)
+
+
+if __name__ == "__main__":
+    openai.api_key = authenticate()
+    while True:
+        chat()
