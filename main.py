298d6fa98afd44529427f4ea969dc0c7b7c6eebb
Sérgio Vorniches <vorniches@gmail.com>
2023-01-25 14:06:00 +0000

    env variable update

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 def authenticate():
     if os.path.exists('.env'):
         load_dotenv()
-        return os.getenv("API_KEY")
+        return os.getenv("OPENAI_API_KEY")
     else:
         return input("Please enter your OpenAI API key: ")
 
